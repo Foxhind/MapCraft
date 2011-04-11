85bd98daac96201098f265efd13e7f08c52b8fcc
Osmisto <osmisto@gmail.com>
2011-04-11 14:20:34 +0400

    handle logic process restarts

--- hub/src/logic_worker.erl
+++ hub/src/logic_worker.erl
@@ -3,7 +3,7 @@
 
 -export([start_link/0]).
 -export([process/2]).
--export([init/1, handle_cast/2]).
+-export([init/1, handle_cast/2, handle_info/2, terminate/2]).
 
 -include("hub.hrl").
 -record(state, {id, port}).
@@ -22,17 +22,28 @@ process(Worker, HubReq) ->
 %% gen_server callbacks
 %%
 init(_Options) ->
-	Id = stats:incr(logic_starts),
-	Port = open_port({spawn, get_logic_cmd(Id)}, [stream, {line, 1000}]),
+	Id = stats:get_next({logic, starts}),
+	Port = open_port({spawn, get_logic_cmd(Id)}, [stream, {line, 1000}, exit_status]),
 	logic:add_me(),
 	{ok, #state{id = Id, port = Port}}.
 
 
 handle_cast({process, HubReq}, State) ->
+	stats:incr({logic, requests}),
 	{ok, Res} = process_req(State#state.port, HubReq#hub_req.cmd),
 	router:spawn_new(HubReq, Res),
 	logic:add_me(),
 	{noreply, State}.
+
+handle_info({Port, {exit_status, Reason}}, #state{port = Port} = State) ->
+	stats:incr({logic, crashes}),
+	error_logger:error_report(["Logic process terminated", {state, State}, {retcode, Reason}]),
+	{stop, {port_terminated, Reason}, State}.
+
+terminate({port_terminated, _}, _) ->
+	ok;
+terminate(_Reason, #state{port = Port}) ->
+	port_close(Port).
 
 %% TODO: trapexit for Port, handle terminate (close port)
 
