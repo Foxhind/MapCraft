afaffa1cc81b114367d992a8e71e711ed36c7b43
Foxhind <foxhind@gmail.com>
2011-04-15 16:28:34 +0400

    Creating pies and implementing list and map views.

--- list.php
+++ list.php
@@ -1,9 +1,17 @@
 <?php
-echo '<table class="list">';
-echo '<tr><th>Название</th><th>Сектора</th><th>Готовность</th><th>Открыт</th><th>Закрыт</th></tr>';
-$m = array(array('Геленджик', '41', '100%', '27 марта 2011', '31 марта 2011'), array('Абакан', '64', '0%', '15 апреля 2011', '—'), array('Котовск', '15', '37%', '12 апреля 2011', '—'));
-foreach ($m as $v) {
-    echo '<tr><td>'.$v[0].'</td><td>'.$v[1].'</td><td>'.$v[2].'</td><td>'.$v[3].'</td><td>'.$v[4].'</td></tr>';
+require 'config.php';
+
+$result = pg_query($connection, 'SELECT pies.id, pies.name, users.nick, pies.start, pies.ends, count(pieces.id) AS num, sum(pieces.state) / (count(pieces.id) * 9.0) * 100 AS state FROM pies INNER JOIN pieces ON pies.id = pieces.pie JOIN users ON pies.author=users.id WHERE pies.visible = true GROUP BY pies.id, pies.name, users.nick, pies.start, pies.ends LIMIT 10');
+
+if (pg_num_rows($result) > 0) {
+    echo '<table class="list">';
+    echo '<tr><th>Название</th><th>Сектора</th><th>Готовность</th><th>Создатель</th><th>Открыт</th><th>Закрыт</th></tr>';
+    while ($row = pg_fetch_array($result)) {
+        echo '<tr><td>'.$row['name'].'</td><td>'.$row['num'].'</td><td>'.round(floatval($row['state'])).'&nbsp;%</td><td>'.$row['nick'].'</td><td>'.$row['start'].'</td><td>'.($row['ends'] ? $row['ends'] : '—').'</td></tr>';
+    }
+    echo '</table>';
 }
-echo '</table>';
+else {
+    echo '<div id="pageheader" style="background-color: #6D926C;">Список пуст</div>';
+}
 ?>
