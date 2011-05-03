4fc39d00082400d594e827308374fd2b744764db
Foxhind <foxhind@gmail.com>
2011-05-03 11:44:07 +0400

    Quickfix for folding/unfolding

--- app/list.php
+++ app/list.php
@@ -14,7 +14,13 @@ if ($start > $piescount or $start < 0)
 $result = pg_query($connection, 'SELECT pies.id, pies.name, pies.description, users.nick, pies.start, pies.ends, count(pieces.id) AS num, sum(pieces.state) / (count(pieces.id) * 9.0) * 100 AS state FROM pies INNER JOIN pieces ON pies.id = pieces.pie JOIN users ON pies.author=users.id WHERE pies.visible = true GROUP BY pies.id, pies.name, pies.description, users.nick, pies.start, pies.ends LIMIT '.$piespp.' OFFSET '.$start);
 
 if (pg_num_rows($result) > 0) {
-    echo '<script>function toggledesc (row) { var rows = row.parentNode.getElementsByClassName("desc"); var thisrow = null; for(var i = 0; i < rows.length; i++) { thisrow = (rows[i] == row.nextSibling); rows[i].style.display = thisrow ? ((rows[i].style.display == "none") ? "table-row" : "none") : "none"; } }</script>';
+    echo '<script>
+    function toggledesc (row) {
+        var rows = row.parentNode.getElementsByClassName("desc");
+        for(var i = 0; i < rows.length; i++)
+            rows[i].style.display = (rows[i] == row.nextSibling) ? ((rows[i].style.display == "table-row") ? "none" : "table-row") : "none";
+    }
+</script>';
     echo '<table class="list">';
     echo '<tr><th>Название</th><th>Сектора</th><th>Готовность</th><th>Создатель</th><th>Открыт</th><th>Закрыт</th></tr>';
     while ($row = pg_fetch_array($result)) {
