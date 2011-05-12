e1a5658fe5296882ce76a6cb882c9ac0138a360b
Foxhind <foxhind@gmail.com>
2011-05-12 18:17:30 +0400

    Initial anchors implementation (direct links to slices)

--- static/js/main.js
+++ static/js/main.js
@@ -742,6 +742,7 @@ function PostComment() {
 function onSelectPiece(e) {
     selectedFeature = e;
     updatePieceStyle(e, true);
+    window.location.hash = e.attributes.name;
     $("#comments").html("<div class='loading'></div>");
     $('#bowner').button("enable");
     $('#bowner').click( function() { $(e.attributes.owner ? (e.attributes.owner == me.nick ? '#drefuse' : '#dclaim') : '#dtake').dialog('open'); });
@@ -828,7 +829,16 @@ $(document).ready(function () {
 
     // Zoom what it will be loaded
     kmllayer.events.register("loadend", this, function() {
-        if (!olmap.getCenter()) {
+        var hash = window.location.hash;
+        hash = hash.charAt(0) == '#' ? hash.substring(1, hash.length) : hash;
+        if (hash != '') {
+            SelectPiece(hash);
+            if (selectedFeature != null) {
+                olmap.zoomToExtent(selectedFeature.geometry.getBounds());
+                olmap.zoomOut();
+            }
+        }
+        else if (!olmap.getCenter()) {
             olmap.zoomToExtent(kmllayer.getDataExtent());
             olmap.zoomOut(); // a bit smaller
         }
