dca722dd61e2d19d742b7ae7873870f008d8c4c7
Osmisto <osmisto@gmail.com>
2011-11-03 16:23:34 +0400

    re #33 - inital version off hub script

--- hub/start-dev.sh
+++ hub/start-dev.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# NOTE: mustache templates need \ because they are not awesome.
+cd $(dirname $0)
+
+PID_FILE=${1:?Pass pid file path as first arg}
+shift
+echo $$ >$PID_FILE
+
 exec erl -pa ebin edit deps/*/ebin -boot start_sasl \
     -sname hub_dev \
     -s hub \
