9c982339d2df63ce3ded9cf7c857b6347fb48223
Osmisto <osmisto@gmail.com>
2011-11-05 00:50:28 +0400

    re #33 - use nodetool to start/stop/control hub

--- hub/start-dev.sh
+++ hub/start-dev.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
-
-PID_FILE=${1:?Pass pid file path as first arg}
-shift
-echo $$ >$PID_FILE
-
 exec erl -pa ebin edit deps/*/ebin -boot start_sasl \
     -sname hub_dev \
     -s hub \
