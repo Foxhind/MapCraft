add84d191e5fa2cf3deb40bb9040d386c3adf138
Osmisto <osmisto@gmail.com>
2012-07-22 10:39:21 -0500

    rearrange create script; add indexes during creation; flag to disable captchas

--- app/create.php
+++ app/create.php
@@ -4,156 +4,19 @@
 require '../lib/update_kml.php';
 require '../lib/create_map.php';
 
+//
+// Deny guests
+//
 $user_id = $_SESSION['user_id'];
-if (isset($user_id)) {
-    if (isset($_POST['captcha'])) {
-        if ($_SESSION['security_code'] != strtolower($_POST['captcha'])) {
-            unset($_SESSION['security_code']);
-            echo 'Incorrect captcha.<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-        unset($_SESSION['security_code']);
-        if ($_FILES['file']['size'] > 524288) {
-            echo 'Too big file. It must be less than 512 kb.<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-        if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
-            echo 'Not specified osm file.<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-        if (!isset($_POST['name']) or empty($_POST['name'])) {
-            echo 'Not specified cake name.<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
+if (!isset($user_id)) {
+    echo 'Anonymous can\'t create cakes. <a href="/app/auth.php?reload=1" target="_blank">Log in</a>, please.';
+    exit();
+}
 
-        $result = pg_fetch_assoc(pg_query($connection, 'SELECT * FROM users WHERE id=\''.$user_id.'\''), 0);
-        if (!$result) {
-            echo 'The user with id =  '.$user_id.' is not present in the base. Please logout and login back. <br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-        $osm_user = pg_fetch_result($result, 0, "nick");
-
-        $coordinates = array();
-        $nodes = array();
-        $currentway = null;
-        $bbox = null;
-        function saxStartElement($parser, $name, $attrs)
-        {
-            global $currentway, $nodes, $bbox;
-            switch($name)
-            {
-                case 'node':
-                    $lon = floatval($attrs['lon']);
-                    $lat = floatval($attrs['lat']);
-                    if ($bbox == null)
-                        $bbox = array($lon, $lat, $lon, $lat);
-                    else {
-                        if ($lon < $bbox[0]) $bbox[0] = $lon;
-                        if ($lon > $bbox[2]) $bbox[2] = $lon;
-                        if ($lat < $bbox[1]) $bbox[1] = $lat;
-                        if ($lat > $bbox[3]) $bbox[3] = $lat;
-                    }
-                    $nodes[$attrs['id']] = $attrs['lon'].','.$attrs['lat'];
-                    break;
-                case 'way':
-                    $currentway = array();
-                    break;
-                case 'nd':
-                    if ($currentway !== null)
-                        $currentway[] = $nodes[$attrs['ref']];
-                    break;
-                case 'tag':
-                    $currentway = null;
-                    break;
-            };
-        }
-
-        function saxEndElement($parser, $name)
-        {
-            global $coordinates, $currentway;
-            if ($name == 'way' and $currentway != null) {
-                if ($currentway[0] == $currentway[count($currentway)-1])
-                    $coordinates[] = implode(' ', $currentway);
-                $currentway = null;
-            }
-        }
-
-        $parser = xml_parser_create();
-        xml_set_element_handler($parser,'saxStartElement','saxEndElement');
-        xml_parser_set_option($parser,XML_OPTION_CASE_FOLDING, false);
-
-        $data = file_get_contents($_FILES['file']['tmp_name']);
-        if (!xml_parse($parser, $data)) {
-            echo 'XML error: '.xml_error_string(xml_get_error_code($parser)).' at line '.xml_get_current_line_number($parser);
-            echo '<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-
-        xml_parser_free($parser);
-
-        if (count($coordinates) < 2) {
-            echo 'Slices count must be over one.<br /><a href="javascript:history.back();">Back</a>';
-            exit();
-        }
-
-        // Adding pie
-        $bboxcenter = json_encode( array(($bbox[0]+$bbox[2])/2, ($bbox[1]+$bbox[3])/2) );
-        $result = pg_query($connection, 'INSERT INTO pies VALUES(DEFAULT, \''.pg_escape_string($_POST['name']).'\', '.pg_escape_string($user_id).', DEFAULT, NULL, \''.pg_escape_string(htmlspecialchars($_POST['description'])).'\', '.(isset($_POST['hide']) ? 'false' : 'true').', \''.$bboxcenter.'\') RETURNING id');
-        $pie_id = pg_fetch_result($result, 0, 0);
-
-        // Adding pieces
-        foreach ($coordinates as $c) {
-            $result = pg_query($connection, 'INSERT INTO pieces VALUES(DEFAULT, DEFAULT, DEFAULT, '.$pie_id.', \''.$c.'\')');
-        }
-
-        // Accesses
-        $raccess = array();
-        $rwaccess = array();
-        foreach (explode(',', $_POST['waccess']) as $username) {
-            $username = trim($username);
-            if (!empty($username) and $username != $osm_user)
-                $rwaccess[] = $username;
-        }
-        foreach (explode(',', $_POST['raccess']) as $username) {
-            $username = trim($username);
-            if (!empty($username) and !in_array($username, $rwaccess) and $username != $osm_user)
-                $raccess[] = $username;
-        }
-        // Adding it to table
-        foreach ($raccess as $username) {
-            $username = pg_escape_string($username);
-            $result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$username.'\'');
-            if (pg_num_rows($result) > 0)
-                $user_id = pg_fetch_result($result, 0 ,0);
-            else
-                $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$username.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
-            $result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$username.'\', \'r\')');
-        }
-        foreach ($rwaccess as $username) {
-            $username = pg_escape_string($username);
-            $result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$username.'\'');
-            if (pg_num_rows($result) > 0)
-                $user_id = pg_fetch_result($result, 0 ,0);
-            else
-                $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$username.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
-            $result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$username.'\', \'rw\')');
-        }
-        // And owner
-        $result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$osm_user.'\'');
-        if (pg_num_rows($result) > 0)
-            $user_id = pg_fetch_result($result, 0 ,0);
-        else
-            $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$osm_user.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
-        $result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$osm_user.'\', \'o\')');
-
-        update_kml($pie_id);
-        create_map($pie_id);
-
-        $pie_link = 'http://'.$_SERVER['HTTP_HOST'].'/pie/'.$pie_id;
-        $wms_link = 'wms:http://'.$_SERVER['HTTP_HOST'].'/wms/'.$pie_id.'?';
-        echo 'Done!<p>Cake link: <a href="'.$pie_link.'" target="_blank">'.$pie_link.'</a></p><p>WMS link: '.$wms_link.'</p>';
-    }
-    else {
+//
+// Draw basic dialog if nothing was submitted yet
+//
+if (!isset($_POST['name'])) {
 ?>
 <form action="" method="post" enctype="multipart/form-data">
 <fieldset>
@@ -179,17 +42,181 @@ if (isset($user_id)) {
     <div class="row"><div>
         <label><input class="btn" type="checkbox" id="hide" name="hide" />Hidden cake<!--<br/><small>This doesn't affect the access!</small>--></label>
     </div></div>
+<?php
+    if (!$skip_captcha) {
+?>
     <div class="row"><div>
         <label for="captcha">Captcha <em>*</em> <img src="/app/captcha.php" /></label>
         <input type="text" id="captcha" name="captcha" required="required" />
     </div></div>
-    <input class="btn" type="submit" value="&nbsp Create »" />
-</fieldset>
 <?php
     }
-}
-else {
-    echo 'Anonymous can\'t create cakes. <a href="/app/auth.php?reload=1" target="_blank">Log in</a>, please.';
-}
 ?>
+    <input class="btn" type="submit" value="&nbsp Create »" />
+</fieldset>
 </form>
+<?php
+    exit();
+}
+
+
+// Check captcha
+if (!$skip_captcha && $_SESSION['security_code'] != strtolower($_POST['captcha'])) {
+    unset($_SESSION['security_code']);
+    echo 'Incorrect captcha.<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+unset($_SESSION['security_code']);
+
+// Check filesize and type
+if ($_FILES['file']['size'] > 524288) {
+    echo 'Too big file. It must be less than 512 kb.<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
+    echo 'Not specified osm file.<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+
+// Require name
+if (!isset($_POST['name']) or empty($_POST['name'])) {
+    echo 'Not specified cake name.<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+
+// Check user
+$result = pg_fetch_assoc(pg_query($connection, 'SELECT * FROM users WHERE id=\''.$user_id.'\''), 0);
+if (!$result) {
+    echo 'The user with id =  '.$user_id.' is not present in the base. Please logout and login back. <br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+$osm_user = pg_fetch_result($result, 0, "nick");
+
+
+// XML parser body
+$coordinates = array();
+$nodes = array();
+$currentway = null;
+$bbox = null;
+function saxStartElement($parser, $name, $attrs)
+{
+    global $currentway, $nodes, $bbox;
+    switch($name)
+    {
+        case 'node':
+            $lon = floatval($attrs['lon']);
+            $lat = floatval($attrs['lat']);
+            if ($bbox == null)
+                $bbox = array($lon, $lat, $lon, $lat);
+            else {
+                if ($lon < $bbox[0]) $bbox[0] = $lon;
+                if ($lon > $bbox[2]) $bbox[2] = $lon;
+                if ($lat < $bbox[1]) $bbox[1] = $lat;
+                if ($lat > $bbox[3]) $bbox[3] = $lat;
+            }
+            $nodes[$attrs['id']] = $attrs['lon'].','.$attrs['lat'];
+            break;
+        case 'way':
+            $currentway = array();
+            break;
+        case 'nd':
+            if ($currentway !== null)
+                $currentway[] = $nodes[$attrs['ref']];
+            break;
+        case 'tag':
+            $currentway = null;
+            break;
+    };
+}
+
+function saxEndElement($parser, $name)
+{
+    global $coordinates, $currentway;
+    if ($name == 'way' and $currentway != null) {
+        if ($currentway[0] == $currentway[count($currentway)-1])
+            $coordinates[] = implode(' ', $currentway);
+        $currentway = null;
+    }
+}
+
+$parser = xml_parser_create();
+xml_set_element_handler($parser,'saxStartElement','saxEndElement');
+xml_parser_set_option($parser,XML_OPTION_CASE_FOLDING, false);
+
+$data = file_get_contents($_FILES['file']['tmp_name']);
+if (!xml_parse($parser, $data)) {
+    echo 'XML error: '.xml_error_string(xml_get_error_code($parser)).' at line '.xml_get_current_line_number($parser);
+    echo '<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+
+xml_parser_free($parser);
+
+if (count($coordinates) < 2) {
+    echo 'Slices count must be over one.<br /><a href="javascript:history.back();">Back</a>';
+    exit();
+}
+
+// Adding pie
+$bboxcenter = json_encode( array(($bbox[0]+$bbox[2])/2, ($bbox[1]+$bbox[3])/2) );
+$result = pg_query($connection, 'INSERT INTO pies VALUES(DEFAULT, \''.pg_escape_string($_POST['name']).'\', '.pg_escape_string($user_id).', DEFAULT, NULL, \''.pg_escape_string(htmlspecialchars($_POST['description'])).'\', '.(isset($_POST['hide']) ? 'false' : 'true').', \''.$bboxcenter.'\') RETURNING id');
+$pie_id = pg_fetch_result($result, 0, 0);
+
+// Adding pieces
+$index = 1;
+foreach ($coordinates as $c) {
+    $result = pg_query($connection, 'INSERT INTO pieces VALUES(DEFAULT, DEFAULT, DEFAULT, '.$pie_id.', \''.$c.'\',' . $index++ . ')');
+}
+
+// Accesses
+$raccess = array();
+$rwaccess = array();
+foreach (explode(',', $_POST['waccess']) as $username) {
+    $username = trim($username);
+    if (!empty($username) and $username != $osm_user)
+        $rwaccess[] = $username;
+}
+foreach (explode(',', $_POST['raccess']) as $username) {
+    $username = trim($username);
+    if (!empty($username) and !in_array($username, $rwaccess) and $username != $osm_user)
+        $raccess[] = $username;
+}
+
+// Adding it to table
+foreach ($raccess as $username) {
+    $username = pg_escape_string($username);
+    $result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$username.'\'');
+    if (pg_num_rows($result) > 0)
+        $user_id = pg_fetch_result($result, 0 ,0);
+    else
+        $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$username.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
+    $result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$username.'\', \'r\')');
+}
+foreach ($rwaccess as $username) {
+    $username = pg_escape_string($username);
+    $result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$username.'\'');
+    if (pg_num_rows($result) > 0)
+        $user_id = pg_fetch_result($result, 0 ,0);
+    else
+        $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$username.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
+    $result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$username.'\', \'rw\')');
+}
+// And owner
+$result = pg_query($connection, 'SELECT id FROM users WHERE nick=\''.$osm_user.'\'');
+if (pg_num_rows($result) > 0)
+    $user_id = pg_fetch_result($result, 0 ,0);
+else
+    $user_id = pg_fetch_result(pg_query($connection, 'INSERT INTO users VALUES(\''.$osm_user.'\', DEFAULT, DEFAULT) RETURNING id'), 0 ,0);
+$result = pg_query($connection, 'INSERT INTO access VALUES('.$user_id.', '.$pie_id.', \''.$osm_user.'\', \'o\')');
+
+update_kml($pie_id);
+create_map($pie_id);
+
+$pie_link = '/pie/'  . $pie_id;
+?>
+
+Done!<br/>
+Please follow <a href="<?=$pie_link;?>">this link</a>, if you have not been forwarded automatically.
+<script type="text/javascript">
+     window.location = "<?=$pie_link;?>";
+</script>
