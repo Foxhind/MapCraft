548758f19a9e0662c62f0cce1d4377c4afa52b75
Andrew Shadura <bugzilla@tut.by>
2013-01-15 01:33:09 +0100

    Add Slovak language.
    --HG--
    rename : static/js/lang/en.js => static/js/lang/sk.js

---
+++ static/js/lang/sk.js
@@ -0,0 +1,46 @@
+var ldata = [
+"Áno",
+"Nie",
+"OK",
+"Zrušiť",
+"Chat",
+"Používatelia",
+"Vlastnosti",
+"Nastavenia
+"Zmeniť meno",
+"Zmeniť stav",
+"Remote",
+"Cake",
+"Žiadný",
+"Prídať",
+"Nové meno:",
+"Vlastník:",
+"Progres:",
+"Komentáre:",
+"Meno",
+"Kúsky",
+"Claims",
+"Štýl:",
+"Jazyk:",
+"Váš komentár",
+"Chceli by ste si zobrať tento kúsok?",
+"Tento kúsok už má vlastníka! Chceli by ste to dať do voľby?",
+"Chceli by ste si odovzdať tento kúsok?",
+"Vitajte v chate, %s!",
+"Zobrazovať mena:",
+"Zobrazovať vlastnené kúsky:"
+];
+
+var trans = [
+'Yes',
+'Áno',
+
+'Piece has been freed',
+'Kúsok bol vrátený',
+
+'Piece has been reserved',
+'Kúsok bol rezervovaný'
+];
+
+for ( i = 0; i < 10 ; i++ )
+    trans.push('New state: ' + i + '/9', 'Nový stav: ' + i + '/9');
